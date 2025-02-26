220bf4f410321b9419ba6225df3aba06be021f35
mykirk98 <mykirk98@gmail.com>
2025-02-26 21:54:26 +0900

    make callback function to class

--- connection_check.py
+++ connection_check.py
@@ -1,40 +1,57 @@
 import stapipy as st
 
-def datastream_callback(handle=None, context=None):
+
+class CMyCallback:
     """
-    Callback to handle events from DataStream
-    
-    handle : handle that trigger the callback
-    context : user data passed on during callback registration
+    Class that contains a callback function
     """
-    
-    st_datastream = handle.module
-    if st_datastream:
-        with st_datastream.retrieve_buffer() as st_buffer:
-            if st_buffer.info.is_image_present == True:
-                st_image = st_buffer.get_image()
-                print(f"BlockID : {st_buffer.info.frame_id} Size : {st_image.width} x {st_image.height} First Byte : {st_image.get_image_data()[0]}")
-                # print(f"BlockID : {st_buffer.info.frame_id} Size : {st_image.shape} First Byte : {st_image.get_image_data()[0]}")
-                # print(f"BlockID : {st_buffer.info.frame_id} Size : {st_image.shape} Byte : {st_image.get_image_data()}")
-            else:
-                # print("Image data does'nt exist")
-                print("Image data does not exist")
+    # def __init__(self):
+    #     self._image = None
+    #     self._lock = threading.Lock()
+
+    def datastream_callback(self, handle=None, context=None):
+        """
+        Callback to handle events from DataStream
+        
+        handle : handle that trigger the callback
+        context : user data passed on during callback registration
+        """
+        
+        st_datastream = handle.module
+        if st_datastream:
+            with st_datastream.retrieve_buffer() as st_buffer:
+                # Check if the acquired data contains image data
+                if st_buffer.info.is_image_present == True:
+                    # Create an image object
+                    st_image = st_buffer.get_image()
+                    print(f"BlockID : {st_buffer.info.frame_id} Size : {st_image.width} x {st_image.height} First Byte : {st_image.get_image_data()[0]}")
+                else:
+                    print("Image data does not exist")
 
 
 if __name__ == "__main__":
+    # Get the callback function
+    my_callback = CMyCallback()
+    callback_func = my_callback.datastream_callback
+    
     try:
         st.initialize()
         st_system = st.create_system()
         st_device = st_system.create_first_device()
         
-        print(f"device : {st_device.info.display_name}")
+        # print(f"device : {st_device.info.display_name}")
+        print(f"model : {st_device.info.model}")
+        print(f"serial number : {st_device.info.serial_number}")
+        print(f"device id : {st_device.info.device_id}")
         
         # Create a datastream object for handling image stream data
         st_datastream = st_device.create_datastream()
         
-        callback = st_datastream.register_callback(datastream_callback)
+        # Register callback for datastream
+        # callback = st_datastream.register_callback(datastream_callback) #NOTE: use when using callback function NOT callback class
+        callback = st_datastream.register_callback(callback_func)
         
-        st_datastream.start_acquisition()
+        st_datastream.start_acquisition(5)
         st_device.acquisition_start()
         
         input("Press enter to terminate")
