81f238c038a9b18b2480a2e19233efd9a3a2b190
mykirk98 <mykirk98@gmail.com>
2025-03-21 08:10:03 +0900

    update Camera class

--- camera.py
+++ camera.py
@@ -2,19 +2,60 @@ import stapipy as st
 # import logging
 
 class Camera:
+    """
+    카메라 클래스
+    """
+    
     def __init__(self, st_system):
-        # self.device = self.st_system.create_first_device()
+        """
+        Args:
+            st_system: system object
+        """
         self.device = st_system.create_first_device()
         self.datastream = self.device.create_datastream()
+        self.runningFlag = None
+    
     
     def start(self):
+        """
+        Start camera acquisition
+        """
+        self.runningFlag = True
+        
         self.datastream.start_acquisition()
         self.device.acquisition_start()
+        
         print(f"Device {self.device.info.display_name} started")
     
+    
+    # def get_image(self):
+    #     """
+    #     Get image from camera
+    #     """
+        
+    #     while self.runningFlag is True:
+    #         with self.datastream.retrieve_buffer() as buffer:   # with ~ as 구문으로 버퍼를 자동으로 해제
+    #             # 버퍼에 이미지가 있는지 확인
+    #             if buffer.info.is_image_present:
+    #                 image = buffer.get_image()
+    #                 print(f"BlockID : {buffer.info.frame_id} Size : {image.width} x {image.height} Bytes : {image.get_image_data()[0]}")
+    #                 # return image
+    #             else:
+    #                 print("Image data does not exist")
+    #                 return 0
+    #         # time.sleep(0.01)
+
+    
     def stop(self):
+        """
+        Stop camera acquisition
+        """
+        
+        self.runningFlag = False
+        
         self.device.acquisition_stop()
         self.datastream.stop_acquisition()
+        
         print(f"Device {self.device.info.display_name} stopped")
 
 
@@ -24,4 +65,6 @@ if __name__ == "__main__":
     
     cam1 = Camera(st_system=st_system)
     cam1.start()
+    cam1.get_image()
+    input("Press enter to terminate\n")
     cam1.stop()
